852e8798035adbec5eb40b23c314d7d227a4ebbe
Erin Eland <erin_168@icloud.com>
2021-02-06 12:22:44 +0000

    Adds component, styles & tests for StarshipDetails

---
+++ src/components/StarshipDetails/StarshipDetails.jsx
@@ -0,0 +1,13 @@
+import './TopTrumpCard.css';
+
+function TopTrumpCard(props) {
+    const { name } = props;
+
+    return (
+      <div className="TopTrumpCard" data-testid="top-trump-card">
+          <h1>{name}</h1>
+      </div>
+    );
+};
+
+export default TopTrumpCard;
