000549e6fdbb1d803ac745bae7b85522cc258d93
Erin Eland <erin@erineland.com>
2021-02-06 21:37:19 +0000

    Make util to find tallest character & unit test.

--- src/utils/decideWinningCard/decideWinningCard.test.js
+++ src/utils/decideWinningCard/decideWinningCard.test.js
@@ -3,7 +3,7 @@ import decideWinningCard from './decideWinningCard';
 describe('Decide Winning Card Utility', () => {
     describe('When deciding between starship type cards', () => {
         it('Should decide that the card with the highest hyperdrive rating is the winner', () => {
-            const exampleStarshipCardsToCompare = [
+            const starships = [
                 {
                     "name": "Sentinel-class landing craft",
                     "model": "Sentinel-class landing craft",
@@ -74,11 +74,129 @@ describe('Decide Winning Card Utility', () => {
                 },
             ]
 
-            const winningCardName = decideWinningCard(exampleStarshipCardsToCompare, 'starship');
+            const winningCardName = decideWinningCard(starships, 'starship');
 
             expect(winningCardName).toEqual('Death Star');
         });
     });
 
     //TODO: When deciding between character type cards.
+    describe('When deciding between different character type cards', () => {
+        it('Should declare the card with the largest height as the winner', () => {
+            const characters = [
+                {
+                    "name": "Luke Skywalker",
+                    "height": "172",
+                    "mass": "77",
+                    "hair_color": "blond",
+                    "skin_color": "fair",
+                    "eye_color": "blue",
+                    "birth_year": "19BBY",
+                    "gender": "male",
+                    "homeworld": "http://swapi.dev/api/planets/1/",
+                    "films": [
+                        "http://swapi.dev/api/films/1/",
+                        "http://swapi.dev/api/films/2/",
+                        "http://swapi.dev/api/films/3/",
+                        "http://swapi.dev/api/films/6/"
+                    ],
+                    "species": [],
+                    "vehicles": [
+                        "http://swapi.dev/api/vehicles/14/",
+                        "http://swapi.dev/api/vehicles/30/"
+                    ],
+                    "starships": [
+                        "http://swapi.dev/api/starships/12/",
+                        "http://swapi.dev/api/starships/22/"
+                    ],
+                    "created": "2014-12-09T13:50:51.644000Z",
+                    "edited": "2014-12-20T21:17:56.891000Z",
+                    "url": "http://swapi.dev/api/people/1/"
+                },
+                {
+                    "name": "C-3PO",
+                    "height": "167",
+                    "mass": "75",
+                    "hair_color": "n/a",
+                    "skin_color": "gold",
+                    "eye_color": "yellow",
+                    "birth_year": "112BBY",
+                    "gender": "n/a",
+                    "homeworld": "http://swapi.dev/api/planets/1/",
+                    "films": [
+                        "http://swapi.dev/api/films/1/",
+                        "http://swapi.dev/api/films/2/",
+                        "http://swapi.dev/api/films/3/",
+                        "http://swapi.dev/api/films/4/",
+                        "http://swapi.dev/api/films/5/",
+                        "http://swapi.dev/api/films/6/"
+                    ],
+                    "species": [
+                        "http://swapi.dev/api/species/2/"
+                    ],
+                    "vehicles": [],
+                    "starships": [],
+                    "created": "2014-12-10T15:10:51.357000Z",
+                    "edited": "2014-12-20T21:17:50.309000Z",
+                    "url": "http://swapi.dev/api/people/2/"
+                },
+                {
+                    "name": "R2-D2",
+                    "height": "96",
+                    "mass": "32",
+                    "hair_color": "n/a",
+                    "skin_color": "white, blue",
+                    "eye_color": "red",
+                    "birth_year": "33BBY",
+                    "gender": "n/a",
+                    "homeworld": "http://swapi.dev/api/planets/8/",
+                    "films": [
+                        "http://swapi.dev/api/films/1/",
+                        "http://swapi.dev/api/films/2/",
+                        "http://swapi.dev/api/films/3/",
+                        "http://swapi.dev/api/films/4/",
+                        "http://swapi.dev/api/films/5/",
+                        "http://swapi.dev/api/films/6/"
+                    ],
+                    "species": [
+                        "http://swapi.dev/api/species/2/"
+                    ],
+                    "vehicles": [],
+                    "starships": [],
+                    "created": "2014-12-10T15:11:50.376000Z",
+                    "edited": "2014-12-20T21:17:50.311000Z",
+                    "url": "http://swapi.dev/api/people/3/"
+                },
+                {
+                    "name": "Darth Vader",
+                    "height": "202",
+                    "mass": "136",
+                    "hair_color": "none",
+                    "skin_color": "white",
+                    "eye_color": "yellow",
+                    "birth_year": "41.9BBY",
+                    "gender": "male",
+                    "homeworld": "http://swapi.dev/api/planets/1/",
+                    "films": [
+                        "http://swapi.dev/api/films/1/",
+                        "http://swapi.dev/api/films/2/",
+                        "http://swapi.dev/api/films/3/",
+                        "http://swapi.dev/api/films/6/"
+                    ],
+                    "species": [],
+                    "vehicles": [],
+                    "starships": [
+                        "http://swapi.dev/api/starships/13/"
+                    ],
+                    "created": "2014-12-10T15:18:20.704000Z",
+                    "edited": "2014-12-20T21:17:50.313000Z",
+                    "url": "http://swapi.dev/api/people/4/"
+                },
+            ];
+
+            const winningCardName = decideWinningCard(characters, 'characters');
+
+            expect(winningCardName).toEqual('Darth Vader');
+        });
+    });
 });
