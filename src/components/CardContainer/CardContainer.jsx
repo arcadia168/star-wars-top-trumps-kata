92b573d731cfe0316373915a19240b77b32baa8e
Erin Eland <erin_168@icloud.com>
2021-02-06 15:08:24 +0000

    Implement choice of card type with tests.

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,6 +1,28 @@
+import React, {
+    useState,
+} from 'react';
+
 function CardContainer(props) {
+    //do bulk of logic in here
+
+    //use state hooks, if deck is chosen, then fetch and show cards and button instead
+    const [deckChosen, setDeckChosen] = useState(false); //TODO: rename to game begun?
+    const [cardType, setCardType] = useState(false);
+
+    //TODO: Fetch all types of deck and dynamically render choice from API!
+    //TODO: Translations?
+
     return (
         <div className="card-container" data-testid="card-container">
+            {!deckChosen && (
+                <div
+                    className="choose-card-type"
+                    data-testid="choose-card-type"
+                >
+                    <button className={"choose-card-type-btn choose-characters"}>Choose Characters</button>
+                    <button className={"choose-card-type-btn choose-characters"}>Choose Starships</button>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -8,5 +8,14 @@ describe('Card Container Component', () => {
             const cardContainerElement = screen.getByTestId('card-container');
             expect(cardContainerElement).toBeInTheDocument();
         });
+
+        describe('When initially rendering the component, at the start of a game', () => {
+            it('Should show the user a choice of which deck to pick', () => {
+                const { unmount } = render(<CardContainer />);
+                const ChooseCardTypeContainer = screen.getByTestId('choose-card-type');
+                expect(ChooseCardTypeContainer).toHaveTextContent('Choose Starships');
+                unmount();
+            });
+        });
     });
 });
