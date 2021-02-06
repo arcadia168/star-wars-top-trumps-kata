f781bd5dc0c545aef0faf1f4b97a1f530b1c287f
Erin Eland <erin@erineland.com>
2021-02-06 11:49:06 +0000

    Show card data title with unit test.

--- src/components/TopTrumpCard/TopTrumpCard.jsx
+++ src/components/TopTrumpCard/TopTrumpCard.jsx
@@ -1,6 +1,9 @@
-function TopTrumpCard() {
+function TopTrumpCard(props) {
+    const { name } = props;
+
     return (
       <div className="TopTrumpCard" data-testid="top-trump-card">
+          <h1>{name}</h1>
       </div>
     );
 };
