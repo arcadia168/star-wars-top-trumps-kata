0de4378ae337fe578d277865f1b564ba3a277f2a
Erin Eland <erin@erineland.com>
2021-02-06 11:49:23 +0000

    Show TopTrumpCard in main App with test.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import TopTrumpCard from './components/TopTrumpCard/TopTrumpCard';
 
+const exampleTopTrumpCardProps = {
+  "name": "Republic attack cruiser",
+  "model": "Senator-class Star Destroyer",
+  "manufacturer": "Kuat Drive Yards, Allanteen Six shipyards",
+  "cost_in_credits": "59000000",
+  "length": "1137",
+  "max_atmosphering_speed": "975",
+  "crew": "7400",
+  "passengers": "2000",
+  "cargo_capacity": "20000000",
+  "consumables": "2 years",
+  "hyperdrive_rating": "1.0",
+  "MGLT": "unknown",
+  "starship_class": "star destroyer",
+  "pilots": [],
+  "films": [
+      "http://swapi.dev/api/films/6/"
+  ],
+  "created": "2014-12-20T19:52:56.232000Z",
+  "edited": "2014-12-20T21:23:49.946000Z",
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world again! Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React now
-        </a>
         <div>
         <h1>Here is the top trump card:</h1>
-        <TopTrumpCard></TopTrumpCard>
+        <TopTrumpCard {...exampleTopTrumpCardProps}></TopTrumpCard>
       </div>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const topTrumpCardElement = screen.getByTestId('top-trump-card');
+  expect(topTrumpCardElement).toBeInTheDocument();
 });
