4be13b1d819433226bc0fa46ec8657d4f2b976bd
Erin Eland <erin@erineland.com>
2021-02-07 11:58:47 +0000

    Improve StarShip Details prop.

--- src/components/StarshipDetails/StarshipDetails.test.js
+++ src/components/StarshipDetails/StarshipDetails.test.js
@@ -2,28 +2,7 @@ import { render, screen } from '@testing-library/react';
 import StarshipDetails from './StarshipDetails';
 
 const exampleStarShipDetails =  {
-    "name": "Star Destroyer",
-    "model": "Imperial I-class Star Destroyer",
-    "manufacturer": "Kuat Drive Yards",
-    "cost_in_credits": "150000000",
-    "length": "1,600",
-    "max_atmosphering_speed": "975",
-    "crew": "47,060",
-    "passengers": "n/a",
-    "cargo_capacity": "36000000",
-    "consumables": "2 years",
-    "hyperdrive_rating": "2.0",
-    "MGLT": "60",
-    "starship_class": "Star Destroyer",
-    "pilots": [],
-    "films": [
-        "http://swapi.dev/api/films/1/",
-        "http://swapi.dev/api/films/2/",
-        "http://swapi.dev/api/films/3/"
-    ],
-    "created": "2014-12-10T15:08:19.848000Z",
-    "edited": "2014-12-20T21:23:49.870000Z",
-    "url": "http://swapi.dev/api/starships/3/"
+    "hyperdriveRating": "2.0",
 };
 
 describe('Starship Details Component', () => {
@@ -38,8 +17,8 @@ describe('Starship Details Component', () => {
         });
 
         it('Should render the starship hyperdrive rating', () => {
-            const CharacterDetailsElement = screen.getByTestId('starship-details');
-            expect(CharacterDetailsElement).toHaveTextContent('2.0');
+            const StarshipDetailsElement = screen.getByTestId('starship-details');
+            expect(StarshipDetailsElement).toHaveTextContent('2.0');
         });
 
         //TODO: Render out other COMPARABLE properties later
